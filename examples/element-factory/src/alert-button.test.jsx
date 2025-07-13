15c776da2871b11fed36784a277d5d991c4be2b3
saadahmadhere <saadahmad728@gmail.com>
2025-07-14 00:18:55 +0530

    alert button test

--- examples/element-factory/src/alert-button.test.jsx
+++ examples/element-factory/src/alert-button.test.jsx
@@ -1,14 +1,35 @@
+import { vi } from 'vitest';
 import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { AlertButton } from './alert-button';
 
-describe.todo('AlertButton', () => {
+describe('AlertButton', () => {
   beforeEach(() => {});
 
   afterEach(() => {});
 
-  it('should render an alert button', async () => {});
+  it.skip('should render an alert button', async () => {});
 
-  it('should trigger an alert', async () => {});
+  it('should trigger an alert', async () => {
+    render(<AlertButton />);
+
+    const alertspy = vi.spyOn(window, 'alert');
+
+    const input = screen.getByLabelText('Message');
+    const button = screen.getByRole('button', { name: /trigger alert/i });
+
+    await act(async () => {
+      await userEvent.clear(input);
+      await userEvent.type(input, 'hello');
+
+      await userEvent.click(button);
+
+      console.log(alertspy);
+
+      expect(alertspy).toHaveBeenCalled();
+      expect(alertspy).toHaveBeenCalledTimes(1);
+      expect(alertspy).toHaveBeenCalledWith('hello');
+    });
+  });
 });
