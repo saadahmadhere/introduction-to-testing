e63e08e3c45d88e1cfdd3ac4d3c6d2d0f5822988
saadahmadhere <saadahmad728@gmail.com>
2025-07-21 13:44:44 +0530

    development mode logging

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,4 +1,22 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-describe.todo('logger', () => {});
+describe('logger', () => {
+  describe('development', () => {
+    beforeEach(() => {
+      vi.stubEnv('MODE', 'development');
+    });
+
+    it('logs to the console in development mode', () => {
+      const logSpy = vi.spyOn(console, 'log');
+      console.log('hi');
+      expect(logSpy).toHaveBeenCalledWith('hi');
+    });
+
+    afterEach(() => {
+      // vi.unstubAllEnvs();
+      vi.restoreAllMocks(); // according to the lecture.
+    });
+  });
+  describe.todo('prod', () => {});
+});
